307a8c8fc7df3be965cb07ca3ddb3dac70fad2d0
Mateus Paranhos <teu_paranhos@outlook.com>
2022-06-13 09:01:06 -0300

    ajustes no endpoint /categorias/{id}

--- src/main/java/com/mateusparanhos/cursomc/CursomcApplication.java
+++ src/main/java/com/mateusparanhos/cursomc/CursomcApplication.java
@@ -1,14 +1,63 @@
 package com.mateusparanhos.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mateusparanhos.cursomc.domain.Categoria;
+import com.mateusparanhos.cursomc.domain.Produto;
+import com.mateusparanhos.cursomc.repositories.CategoriaRepository;
+import com.mateusparanhos.cursomc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List test = new ArrayList<Categoria>();
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		test.add(cat1);
+		test.add(cat2);
+
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2 , p3));
+		
+		
+		
+	}
+	
+	
 
 }
 
---
+++ src/main/java/com/mateusparanhos/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.mateusparanhos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateusparanhos.cursomc.domain.Categoria;
+import com.mateusparanhos.cursomc.repositories.CategoriaRepository;
+import com.mateusparanhos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria>obj = repo.findById(id);
+		return  obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo:" + Categoria.class.getName()));
+		
+	}
+
+}
