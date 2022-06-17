06af7d2cade99137dc621c22a84d5ce65dce819c
Mateus Paranhos <teu_paranhos@outlook.com>
2022-06-17 15:01:40 -0300

    Correcoes em Categoriaservice e construtor de Pagamento

--- src/main/java/com/mateusparanhos/cursomc/services/CategoriaService.java
+++ src/main/java/com/mateusparanhos/cursomc/services/CategoriaService.java
@@ -35,9 +35,11 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
+	
 	
 	public void delete(Integer id) {
 		find(id);
@@ -63,5 +65,8 @@ public class CategoriaService {
 
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
